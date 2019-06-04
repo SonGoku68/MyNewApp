742311b16aefcca5fa9ffbd520c1bf21bfae8147
Fran Romero <francisco@nutmeg.com>
2019-06-04 15:40:27 +0100

    My nasty commit

--- MyNewApp/MyNewApp/ViewController.swift
+++ MyNewApp/MyNewApp/ViewController.swift
@@ -32,5 +32,65 @@ class ViewController: UIViewController {
         }
     }
 
+    public func thisIsALongAndRepeatedFunction() {
+        let myProp: String = "My new string"
+
+        if !myProp.isEmpty {
+            if myProp == "My old string" {
+                print(myProp)
+            } else {
+                if myProp.index(myProp.startIndex, offsetBy: 2) == "s" {
+                    print("\(myProp) is right")
+                } else {
+                    print(myProp)
+                }
+            }
+        }
+        if !myProp.isEmpty {
+            if myProp == "My old string" {
+                print(myProp)
+            } else {
+                if myProp.index(myProp.startIndex, offsetBy: 2) == "s" {
+                    print("\(myProp) is right")
+                } else {
+                    print(myProp)
+                }
+            }
+        }
+        if !myProp.isEmpty {
+            if myProp == "My old string" {
+                print(myProp)
+            } else {
+                if myProp.index(myProp.startIndex, offsetBy: 2) == "s" {
+                    print("\(myProp) is right")
+                } else {
+                    print(myProp)
+                }
+            }
+        }
+        if !myProp.isEmpty {
+            if myProp == "My old string" {
+                print(myProp)
+            } else {
+                if myProp.index(myProp.startIndex, offsetBy: 2) == "s" {
+                    print("\(myProp) is right")
+                } else {
+                    print(myProp)
+                }
+            }
+        }
+        if !myProp.isEmpty {
+            if myProp == "My old string" {
+                print(myProp)
+            } else {
+                if myProp.index(myProp.startIndex, offsetBy: 2) == "s" {
+                    print("\(myProp) is right")
+                } else {
+                    print(myProp)
+                }
+            }
+        }
+    }
+
 }
 
